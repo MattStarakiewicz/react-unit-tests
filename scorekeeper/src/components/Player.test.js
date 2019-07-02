4990b27f7ab5e8e45aeb15c02800128c7bc3a59e
MattStarakiewicz <37964428+MattStarakiewicz@users.noreply.github.com>
2019-07-02 09:59:40 +0200

    Add tests to player component

--- scorekeeper/src/components/Player.test.js
+++ scorekeeper/src/components/Player.test.js
@@ -6,6 +6,12 @@ it('renders without crashing', () => {
   shallow(<Player />);
 });
 
+it('matches snapshot', () => {
+    const component = shallow(<Player />);
+
+    expect(component).toMatchSnapshot();
+});
+
 it('renders correct name', () => {
     const playerNamePassed = 'Ania';
     const playerComponent = shallow(<Player name={playerNamePassed} />);
@@ -36,11 +42,30 @@ it('should call onPlayerScoreChange with 1 when plus button is clicked', () => {
 });
 
 it('should call onPlayerScoreChange with -1 when minus button is clicked', () => {
-  const mockedOnPlayerScoreChange = jest.fn();
-  const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+    const mockedOnPlayerScoreChange = jest.fn();
+    const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
 
-  const minusButton = playerComponent.find('.Player__button').at(1);
-  minusButton.simulate('click');
+    const minusButton = playerComponent.find('.Player__button').at(1);
+    minusButton.simulate('click');
 
-  expect(mockedOnPlayerScoreChange).toBeCalledWith(-1);
+    expect(mockedOnPlayerScoreChange).toBeCalledWith(-1);
+});
+
+it('should call onPlayerRemove when remove button is clicked', () => {
+    const mockedOnPlayerRemove = jest.fn();
+    const playerComponent = shallow(<Player onPlayerRemove={mockedOnPlayerRemove} />);
+
+    const removeButton = playerComponent.find('.Player__button').at(2);
+    removeButton.simulate('click');
+
+    expect(mockedOnPlayerRemove).toHaveBeenCalled();
+});
+
+it('contains remove button', () => {
+    const mockedOnPlayerRemove = jest.fn();
+    const playerComponent = shallow(<Player onPlayerRemove={mockedOnPlayerRemove} />);
+
+    const text = playerComponent.text();
+
+    expect(text).toContain("Remove");
 });
