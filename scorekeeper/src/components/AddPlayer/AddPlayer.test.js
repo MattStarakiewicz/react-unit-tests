f766fa5c8c248840ae389d6b4b3a6082ce9dd6f5
MattStarakiewicz <37964428+MattStarakiewicz@users.noreply.github.com>
2019-07-02 09:52:34 +0200

    Add new tests to addPlayer

--- scorekeeper/src/components/AddPlayer/AddPlayer.test.js
+++ scorekeeper/src/components/AddPlayer/AddPlayer.test.js
@@ -6,6 +6,31 @@ it('renders without crashing', () => {
   shallow(<AddPlayer />);
 });
 
+it('matches snapshot', () => {
+    const component = shallow(<AddPlayer />);
+
+    expect(component).toMatchSnapshot();
+});
+
+it('contains a submit button', () => {
+    const addPlayerComponent = shallow(<AddPlayer />);
+
+    const addPlayer = addPlayerComponent.find('.AddPlayer__submit').first();
+    const type = addPlayer.prop("type");
+
+    expect(type).toEqual("submit");
+});
+
+it('submit adds a new player', () => {
+    const onPlayerAdd = jest.fn();
+    const addPlayerComponent = mount(<AddPlayer onPlayerAdd={onPlayerAdd} />);
+
+    const form = addPlayerComponent.find('form');
+    form.simulate('submit');
+
+    expect(onPlayerAdd).toHaveBeenCalled();
+});
+
 it('renders correct name', () => {
     const onPlayerAdd = jest.fn();
     const addPlayerComponent = mount(<AddPlayer onPlayerAdd={onPlayerAdd} />);
